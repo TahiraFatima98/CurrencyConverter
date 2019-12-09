0593981fa09351bcd1ecd8cb57282a83bcb77756
Syeda Tahira Fatima <55763747+TahiraFatima98@users.noreply.github.com>
2019-12-09 14:35:11 +0500

    Update CC.java

--- CC.java
+++ CC.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.text.DecimalFormat;
 public class CC {
     void method() {
-        double amount, dollar, pound, code, euro, yen, ringgit, rupee;
+        double amount, dollar, pound, code, euro, rupee;
 
         DecimalFormat f = new DecimalFormat("##.##");
         Scanner sc = new Scanner(System.in);
@@ -11,7 +11,7 @@ public class CC {
 
         System.out.println("which currency You want to Convert ? ");
 
-        System.out.println("1:Ruppe \t2:Dollar \t3:Pound \n4:Euro \t5:Yen \t6:Ringgit ");
+        System.out.println("1:Ruppe \t2:Dollar \t3:Pound \n4:Euro ");
         code = sc.nextInt();
 
         System.out.println("How much Money you want to convert ?");
@@ -28,12 +28,7 @@ public class CC {
 
             euro = amount / 80;
             System.out.println("Your " + amount + " Rupee is : " + f.format(euro) + " Euro");
-
-            yen = amount / 0.63;
-            System.out.println("Your " + amount + " Rupee is : " + f.format(yen) + " Yen");
-
-            ringgit = amount / 16;
-            System.out.println("Your " + amount + " Rupee is : " + f.format(ringgit) + " ringgit");
+            
         } else if (code == 2) {
             // For Dollar Conversion
 
@@ -45,12 +40,7 @@ public class CC {
 
             euro = amount * 0.87;
             System.out.println("Your " + amount + " Dollar is : " + f.format(euro) + " Euro");
-
-            yen = amount * 111.087;
-            System.out.println("Your " + amount + " Dollar is : " + f.format(yen) + " Yen");
-
-            ringgit = amount * 4.17;
-            System.out.println("Your " + amount + " Dollar is : " + f.format(ringgit) + " ringgit");
+            
         } else if (code == 3) {
             // For Pound Conversion
 
@@ -62,12 +52,7 @@ public class CC {
 
             euro = amount * 1.10;
             System.out.println("Your " + amount + " pound is : " + f.format(euro) + " Euro");
-
-            yen = amount * 140.93;
-            System.out.println("Your " + amount + " pound is : " + f.format(yen) + " Yen");
-
-            ringgit = amount * 5.29;
-            System.out.println("Your " + amount + " pound is : " + f.format(ringgit) + " ringgit");
+            
         } else if (code == 4) {
             // For Euro Conversion
 
@@ -80,46 +65,6 @@ public class CC {
             pound = amount * 0.90;
             System.out.println("Your " + amount + " euro is : " + f.format(pound) + " Pound");
 
-            yen = amount * 127.32;
-            System.out.println("Your " + amount + " euro is : " + f.format(yen) + " Yen");
-
-            ringgit = amount * 4.78;
-            System.out.println("Your " + amount + " euro is : " + f.format(ringgit) + " ringgit");
-        } else if (code == 5) {
-
-            // For Yen Conversion
-
-            rupee = amount * 0.63;
-            System.out.println("Your " + amount + " yen is : " + f.format(rupee) + " Ruppes");
-
-            dollar = amount * 0.008;
-            System.out.println("Your " + amount + " yen is : " + f.format(dollar) + " Dollar");
-
-            pound = amount * 0.007;
-            System.out.println("Your " + amount + " yen is : " + f.format(pound) + " Pound");
-
-            euro = amount * 0.007;
-            System.out.println("Your " + amount + " yen is : " + f.format(euro) + " Euro");
-
-            ringgit = amount * 0.037;
-            System.out.println("Your " + amount + " yen is : " + f.format(ringgit) + " ringgit");
-        } else if (code == 6) {
-            // For Ringgit Conversion
-
-            rupee = amount * 16.8;
-            System.out.println("Your " + amount + " ringgit is : " + f.format(rupee) + " Ruppes");
-
-            dollar = amount * 0.239;
-            System.out.println("Your " + amount + " ringgit is : " + f.format(dollar) + " dollar");
-
-            pound = amount * 0.188;
-            System.out.println("Your " + amount + " ringgit is : " + f.format(pound) + " pound");
-
-            euro = amount * 0.209;
-            System.out.println("Your " + amount + " ringgit is : " + f.format(euro) + " euro");
-
-            yen = amount * 26.63;
-            System.out.println("Your " + amount + " ringgit is : " + f.format(yen) + " yen");
         } else {
             System.out.println("Invalid input");
         }
